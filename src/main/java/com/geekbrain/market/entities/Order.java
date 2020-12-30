954290d646a1f5ba36fec0f87de16553e45f2b9c
YulDmi <barsetca@rambler.ru>
2020-12-30 20:20:39 +0300

    builder

--- src/main/java/com/geekbrain/market/entities/Order.java
+++ src/main/java/com/geekbrain/market/entities/Order.java
@@ -3,6 +3,7 @@ package com.geekbrain.market.entities;
 import com.geekbrain.market.utils.Cart;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
@@ -45,4 +46,26 @@ public class Order {
       //  cart.clear();
     }
 
+    public Order fromUser(User user){
+        this.user = user;
+        return this;
+    }
+
+    public Order price(Cart cart){
+        this.price = cart.getPrice();
+        return this;
+    }
+
+    public Order items(Cart cart){
+        cart.getItems().stream().forEach(oi -> {
+            oi.setOrder(this);
+            items.add(oi);
+        });
+        return this;
+    }
+
+    public Order address(String address){
+        this.address = address;
+        return this;
+    }
 }
